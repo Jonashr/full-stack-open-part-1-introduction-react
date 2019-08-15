13709936181bbb63fcc9ef068fd666c4618c7b72
Jonas Horvei <jonashorvei1@gmail.com>
2019-08-15 10:54:37 +0200

    Course information part 4, very primitive use of arrays that will soon be fixed in the next part

--- src/index.js
+++ src/index.js
@@ -16,40 +16,40 @@ const Part = (props) => {
 const Content = (props)=> {
     return (
         <div>
-        <Part part={props.part1.name} exercises={props.part1.exercises} />
-        <Part part={props.part2.name} exercises={props.part2.exercises} />
-        <Part part={props.part3.name} exercises={props.part3.exercises} />
+        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
         </div>
     )
 }
 
 const Total = (props) => {
     return (
-        <p>{props.exercises}</p>
+        <p>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     )
 }
 
 const App = () => {
     const course = 'Half stack appliation development'
-    const part1 = {
-        name: 'Fundamentals of react',
-        exercises: 10
-    }
-
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name: 'Fundamentals of react',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (<div>
                 <Header course={course} />
-                <Content part1={part1} part2={part2} part3={part3}/>
-                <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+                <Content parts={parts}part1={parts[0]} part2={parts[1]} part3={parts[2]}/>
+                <Total parts={parts} />
             </div>
     )
 }
